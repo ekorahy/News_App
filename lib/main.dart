00418ce39af91c53414bf35926ff9b7575f6ffbc
ekorahy <ekorahy@gmail.com>
2022-08-31 09:12:04 +0700

    add Fortnightly theme

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:dicoding_news_app/article.dart';
+import 'package:dicoding_news_app/detail_page.dart';
+import 'package:flutter/material.dart';
+import 'package:dicoding_news_app/styles.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      theme: ThemeData(
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+          primary: primaryColor,
+          onPrimary: Colors.black,
+          secondary: secondaryColor,
+        ),
+        textTheme: myTextTheme,
+        appBarTheme: AppBarTheme(elevation: 0.0),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: secondaryColor,
+            onPrimary: Colors.white,
+            textStyle: const TextStyle(),
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(0),
+              ),
+            ),
+          ),
+        ),
+      ),
+      initialRoute: NewsListPage.routeName,
+      routes: {
+        NewsListPage.routeName: (context) => const NewsListPage(),
+        ArticlesDetailPage.routeName: (context) => ArticlesDetailPage(
+          article: ModalRoute.of(context)?.settings.arguments as Article,
+        ),
+        ArticleWebView.routeName: (context) => ArticleWebView(
+          url: ModalRoute.of(context)?.settings.arguments as String,
+        ),
+      },
+    );
+  }
+}
+
+class NewsListPage extends StatelessWidget {
+  static const routeName = '/article_list';
+  
+  const NewsListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News App'),
+      ),
+      body: FutureBuilder<String>(
+        future:
+          DefaultAssetBundle.of(context).loadString('assets/articles.json'),
+        builder: (context, snapshot) {
+          final List<Article> articles = parseArticles(snapshot.data);
+          return ListView.builder(
+            itemCount: articles.length,
+            itemBuilder: (context, index) {
+              return _buildArticleItem(context, articles[index]);
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _buildArticleItem(BuildContext context, Article article) {
+    return ListTile(
+      contentPadding:
+        const EdgeInsets.symmetric(horizontal: 16.0),
+      leading: Image.network(
+        article.urlToImage,
+        width: 100,
+      ),
+      title: Text(article.title),
+      subtitle: Text(article.author),
+      onTap: () {
+        Navigator.pushNamed(context, ArticlesDetailPage.routeName,
+          arguments: article);
+      },
+    );
+  }
+}
